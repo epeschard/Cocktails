801055f1e547eed89085b973600afbd16c4877ab
clipAI <122525597+clipAI@users.noreply.github.com>
2023-09-23 15:38:09 +0200

    initial commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Eugène Peschard on 23/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
