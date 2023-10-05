d94b8c87c7247a6ab7228a2adc241a691bc63373
Eugène Peschard <eugene@peschard.me>
2023-10-05 16:33:28 +0200

    Add hero animation for title label too

--- Cocktails/Detail/ImageCell.swift
+++ Cocktails/Detail/ImageCell.swift
@@ -4,8 +4,6 @@ import UIKit
 final class ImageCell: UITableViewCell {
   
   var poster: UIImageView!
-  var attribution = UILabel()
-  var creativeCommons = UIImageView()
   
   var onReuse: () -> Void = {}
 
@@ -14,8 +12,6 @@ final class ImageCell: UITableViewCell {
     
     onReuse()
     poster.image = nil
-    attribution.text = ""
-    creativeCommons.image = nil
   }
   
   static var reuseIdentifier: String {
@@ -38,42 +34,22 @@ final class ImageCell: UITableViewCell {
     let poster = UIImageView()
     poster.translatesAutoresizingMaskIntoConstraints = false
     poster.contentMode = .scaleAspectFit
-    attribution.translatesAutoresizingMaskIntoConstraints = false
-    attribution.font = UIFont.preferredFont(forTextStyle: .body)
-    creativeCommons.translatesAutoresizingMaskIntoConstraints = false
     self.poster = poster
           
     // Add the UI components
     contentView.addSubview(poster)
-    contentView.addSubview(attribution)
-    contentView.addSubview(creativeCommons)
     
     selectionStyle = .gray
           
     NSLayoutConstraint.activate([
       poster.widthAnchor.constraint(equalTo: contentView.widthAnchor),
-      poster.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.0),
-      poster.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-      poster.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+      poster.heightAnchor.constraint(equalTo: contentView.widthAnchor),
       poster.topAnchor.constraint(equalTo: contentView.topAnchor),
       poster.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-      attribution.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 8),
-      attribution.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8),
-      attribution.leadingAnchor.constraint(greaterThanOrEqualTo: creativeCommons.trailingAnchor),
-      attribution.widthAnchor.constraint(lessThanOrEqualTo: contentView.widthAnchor),
-      attribution.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
-      creativeCommons.heightAnchor.constraint(equalToConstant: 25),
-      creativeCommons.widthAnchor.constraint(equalToConstant: 25),
-      creativeCommons.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-      creativeCommons.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
     ])
   }
   
   func configure(with drink: Drink) {
-    attribution.text = drink.strImageAttribution ?? ""
-    creativeCommons.image = UIImage(named: "cc-sticker")
-    creativeCommons.isHidden = drink.strCreativeCommonsConfirmed == nil
-    
     let placeholderImage = UIImage(systemName: "wineglass")
     guard
       let string = drink.thumbnail,
